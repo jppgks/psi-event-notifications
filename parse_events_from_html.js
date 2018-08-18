0ef321d375a5594e7371804edb607c66c8b522e4
Joppe Geluykens <joppe.geluykens@student.kuleuven.be>
2018-08-18 19:17:24 +0200

    send mail when seeing new date

--- parse_events_from_html.js
+++ parse_events_from_html.js
@@ -1,20 +1,77 @@
-const tabletojson = require('tabletojson')
-var CronJob = require('cron').CronJob;
+#!/usr/bin/env node
+
+const fs = require('fs');
+const tabletojson = require('tabletojson');
+const CronJob = require('cron').CronJob;
 const request = require('request');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const config = require('./config');
+
+let table_to_json = function (event_table, $) {
+    event_table = $('<table>').append(event_table);
+    event_table = tabletojson.convert(event_table.html());
+    return event_table[0];
+};
+
+let send_mail = function () {
+    let transporter = nodemailer.createTransport({
+        host: 'smtps.kuleuven.be',
+        port: 465,
+        secure: true,
+        auth: {
+            user: "r0586832",
+            pass: config.password 
+        }
+    });
+
+    let mailOptions = {
+        from: '"Joppe Geluykens" <joppe.geluykens@student.kuleuven.be>',
+        to: 'joppe.geluykens@student.kuleuven.be', // list of receivers
+        subject: 'New PSI Seminar 🎟',
+        text: 'https://www.esat.kuleuven.be/psi/seminars/overview', // plain text body
+        html: '<a href="https://www.esat.kuleuven.be/psi/seminars/overview">See upcoming seminars</a>' // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+    });
+};
 
 function on_cron_tick() {
-	var raw_html = ""
-	request('https://www.esat.kuleuven.be/psi/seminars/seminar-archive', function (error, response, html) {
-	  if (!error && response.statusCode == 200) {
-		raw_html = html;
-		const $ = cheerio.load(raw_html)
-		var event_table = $('table[summary="Content listing"]')
-		event_table = $('<table>').append(event_table)
-		event_table = tabletojson.convert(event_table.html())
-		console.log(event_table)
-	  }
-	});
+    request('https://www.esat.kuleuven.be/psi/seminars/seminar-archive', function (error, response, html) {
+        if (!error && response.statusCode === 200) {
+            // Download webpage
+            const $ = cheerio.load(html);
+
+            // Find event table
+            let event_table = $('table[summary="Content listing"]');
+
+            // Convert event table to JSON
+            const events = table_to_json(event_table, $);
+
+            // Read date of last seen event from file
+            let date_last_seen_filepath = "date_most_recent_event_seen.txt";
+            fs.readFile(date_last_seen_filepath, function (err, data) {
+                const most_recent_event = events[0];
+                const most_recent_event_date = most_recent_event['Start Date'];
+
+                if (data.toString() !== most_recent_event_date) {
+                    // Write date of most recent event to file
+                    fs.writeFile(date_last_seen_filepath, most_recent_event_date, function (err) {
+                        console.debug("Wrote date of most recent event to file.")
+                    });
+
+                    // Send email
+                    send_mail();
+                }
+            });
+        }
+    });
 }
 
 console.log('Before job instantiation');
@@ -22,4 +79,3 @@ console.log('Before job instantiation');
 const job = new CronJob('* * * * * *', on_cron_tick);
 console.log('After job instantiation');
 job.start();
-
