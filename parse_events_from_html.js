fd521cd1a07096286cf05275bbadfb2af9bf070e
Joppe Geluykens <joppe.geluykens@student.kuleuven.be>
2018-08-18 17:56:33 +0200

    run as cron job

--- parse_events_from_html.js
+++ parse_events_from_html.js
@@ -1,13 +1,22 @@
+var CronJob = require('cron').CronJob;
 const request = require('request');
 const cheerio = require('cheerio')
 
-var raw_html = ""
-request('https://www.esat.kuleuven.be/psi/seminars/seminar-archive', function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    raw_html = html;
-	const $ = cheerio.load(raw_html)
-	var event_table = $('table[summary="Content listing"]')
-	console.log(event_table.html())
-  }
-});
+function on_cron_tick() {
+	var raw_html = ""
+	request('https://www.esat.kuleuven.be/psi/seminars/seminar-archive', function (error, response, html) {
+	  if (!error && response.statusCode == 200) {
+		raw_html = html;
+		const $ = cheerio.load(raw_html)
+		var event_table = $('table[summary="Content listing"]')
+		console.log(event_table.html())
+	  }
+	});
+}
+
+console.log('Before job instantiation');
+//const job = new CronJob('00 30 11 * * 1-5', on_cron_tick);
+const job = new CronJob('* * * * * *', on_cron_tick);
+console.log('After job instantiation');
+job.start();
 
